17f02b4e7d4a93aaa31e8b8dfb2628060d46ef73
Chiheng <lichiheng1998@gmail.com>
2023-03-21 17:04:38 +0800

    first draft

---
+++ server.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from flask import request
+import os
+
+hostName = "localhost"
+serverPort = 8080
+
+image_folder = ".\\output_compressed_images"
+images = []
+
+# iterate over files in
+# that directory
+for filename in os.listdir(image_folder):
+    f = os.path.join(image_folder, filename)
+    # checking if it is a file
+    if os.path.isfile(f):
+        images.append(filename)
+
+app = Flask(__name__)
+
+@app.route('/images', methods = ['POST'])
+def get_images():
+
+    page = int(request.json.get('page'))
+    print(request.json)
+    print("wocaonima")
+    offset = page * 100
+
+    num_images = len(images)
+
+    if offset < num_images:
+        return {
+            "id": images[offset: min(offset + 100, num_images)]
+        }
+    
+    return { "id": [] }
+
+app.run(debug = True)
